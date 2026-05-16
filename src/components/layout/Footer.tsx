9c601610fff697b22449470403d5e7fe72729927
mighty808 <effahaboagye888@gmail.com>
2026-05-16 17:17:35 +0000

    add layout Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="h-10 bg-white border-t border-gray-200 flex items-center justify-center">
+      <p className="text-xs text-gray-400">© 2025 Chelsea Bank. All rights reserved.</p>
+    </footer>
+  )
+}
