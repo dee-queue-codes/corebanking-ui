1db481b09116a3e75f0181bc2d81e26cbb7c65a6
mighty808 <effahaboagye888@gmail.com>
2026-05-16 17:16:21 +0000

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    allowedHosts: ['clifton-fibromatous-candelaria.ngrok-free.dev'],
+    port: 3000,
+  },
+})
